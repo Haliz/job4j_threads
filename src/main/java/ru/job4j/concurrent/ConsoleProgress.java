fbe41d443ff1190cd6de02346c88366ed367dfda
Haliz <foril2n1@rambler.ru>
2022-01-05 21:03:51 +0300

    3. Прерывание нити

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        char[] symbol = {'|', '/', '—', '\\'};
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                for (int i = 0; i < 4; i++) {
+                    System.out.print("\r load: " + symbol[i]);
+                    Thread.sleep(500);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+}
