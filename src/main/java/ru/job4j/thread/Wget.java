3a768be113e9263ce677f4878901ef0faaf9bc99
Haliz <foril2n1@rambler.ru>
2022-01-08 20:15:19 +0300

    4. Скачивание файла с ограничением.

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,47 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[speed];
+            int bytesRead;
+            long startTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                if (bytesRead >= speed) {
+                    long stopTime = System.currentTimeMillis();
+                    long time = stopTime - startTime;
+                    if (time < 1000) {
+                        Thread.sleep((1000 - time));
+                        startTime = System.currentTimeMillis();
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
