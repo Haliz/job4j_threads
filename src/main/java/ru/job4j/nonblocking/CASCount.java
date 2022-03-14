a3a54038fa42d2fae882e1d9729e86c6e402b124
Haliz <foril2n1@rambler.ru>
2022-03-14 22:49:01 +0300

    CAS - операции

---
+++ src/main/java/ru/job4j/nonblocking/CASCount.java
@@ -0,0 +1,24 @@
+package ru.job4j.nonblocking;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int ref;
+        int newValue;
+        do {
+            ref = count.get();
+            newValue = ref + 1;
+        } while (!count.compareAndSet(ref, newValue));
+
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
